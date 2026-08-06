507046686cb5cb98da626f7631d1b4bac97b0d98
oritri98 <ismatsiddiquee98@gmail.com>
2026-08-07 00:43:49 +0600

    new file added

---
+++ adjustment.ino
@@ -0,0 +1,14 @@
+String parameter_list(byte a) {
+  if (a == 1) return "Speed           ";
+  else if (a == 2) return "Rotate Speed    ";
+  else if (a == 3) return "Inverse Delay   ";
+  else if (a == 4) return "KP              ";
+  else if (a == 5) return "KD              ";
+  else if (a == 6) return "T-Junc Rule     ";
+  else if (a == 7) return "Cross Rule      ";
+  else if (a == 8) return "Turn Delay      ";
+  else if (a == 9) return "Brake Delay     ";
+  else if (a == 10) return "U Turn Delay    ";
+  else if (a == 11) return "Stop Delay      ";
+  else if (a == 12) return "Forward Delay   ";
+}
