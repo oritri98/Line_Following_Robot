1d65d1550b25ced6213d081f204a0018700af102
oritri98 <ismatsiddiquee98@gmail.com>
2026-08-06 17:49:32 +0600

    turns updated, new function added and tested

--- turns.ino
+++ turns.ino
@@ -6,3 +6,10 @@ void turnLeft(int L, int R) {
   direction = "straight";
 }
 
+void turnRight(int L, int R) {
+  while (sensorDigital[3] != 1) {
+    motor(R, -L);
+    read_black_line();
+  }
+  direction = "straight";
+}
