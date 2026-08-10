538ba5e7c304207160e047d0b91786a23c4c3d6e
oritri98 <ismatsiddiquee98@gmail.com>
2026-08-11 05:16:41 +0600

    updated

--- adjustment.ino
+++ adjustment.ino
@@ -12,3 +12,21 @@ String parameter_list(byte a) {
   else if (a == 11) return "Stop Delay      ";
   else if (a == 12) return "Forward Delay   ";
 }
+
+int get_eeprom_address(int param_index) {
+  switch(param_index) {
+    case 1: return 43;
+    case 2: return 44;
+    case 3: return 45;
+    case 4: return 46;
+    case 5: return 47;
+    case 6: return 53;
+    case 7: return 54;
+    case 8: return 48;
+    case 9: return 49;
+    case 10: return 50;
+    case 11: return 51;
+    case 12: return 52;
+    default: return 43;
+  }
+}
