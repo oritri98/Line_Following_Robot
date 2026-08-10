ec0110b5b5163bc3dccf289cc24603c8a9185f3c
oritri98 <ismatsiddiquee98@gmail.com>
2026-08-11 05:17:50 +0600

    commit updated

--- adjustment.ino
+++ adjustment.ino
@@ -30,3 +30,157 @@ int get_eeprom_address(int param_index) {
     default: return 43;
   }
 }
+
+void edit_parameter() {
+  oled.set1X();
+  byte base = 1, peak = 8, temp = 0, ind = 1, limit = 12;
+  int value;
+
+  while (1) {
+    if (temp != ind) {
+      temp = ind;
+      for (byte i = 0; i < 8; i++) {
+        if (i + base > limit) break;
+        (i == ind - base) ? oled.setInvertMode(1) : oled.setInvertMode(0);
+        
+        if (i + base == 6 || i + base == 7) {
+          int eeprom_addr = get_eeprom_address(i + base);
+          value = EEPROM.read(eeprom_addr);
+          String ruleText = (value == 0) ? "Left" : "Right";
+          String paramName = parameter_list(i + base);
+          text(String() + "->" + paramName + ruleText + "    ", 2, i);
+        } else {
+          int eeprom_addr = get_eeprom_address(i + base);
+          value = EEPROM.read(eeprom_addr);
+          text(String() + "->" + parameter_list(i + base) + String(value) + "   ", 2, i);
+        }
+      }
+    }
+
+    byte x = push(button3);
+    byte r = push(button1);
+
+    if (x) {
+      if (x == 1) {
+        ind++;
+        if (ind > limit) {
+          ind = 1;
+          base = ind;
+          peak = base + 7;
+        } else if (ind > peak) {
+          peak = ind;
+          base = peak - 7;
+        }
+      }
+    }
+
+    if (r) {
+      if (r == 1) {
+        ind--;
+        if (ind < 1) {
+          ind = limit;
+          peak = ind;
+          base = peak - 7;
+        } else if (ind < base) {
+          base = ind;
+          peak = base + 7;
+        }
+      }
+    }
+
+    if (r == 2) {
+      set_adjust(ind);
+      temp = 0;
+    }
+
+    if (x == 2) {
+      eeprom_refresh();
+      if (t_hand_rule == 0) {
+        t_scan = "left";
+      } else {
+        t_scan = "right";
+      }
+      
+      if (cross_hand_rule == 0) {
+        cross_scan = "left";
+      } else {
+        cross_scan = "right";
+      }
+      return;
+    }
+  }
+}
+
+void set_adjust(int indicator) {
+  oled.set2X();
+  oled.clear();
+
+  int value;
+  int eeprom_addr = get_eeprom_address(indicator);
+  
+  if (indicator == 6 || indicator == 7) {
+    value = EEPROM.read(eeprom_addr);
+    String ruleText = (value == 0) ? "Left" : "Right";
+    String paramName = (indicator == 6) ? "T-Junc Rule" : "Cross Rule";
+    
+    int temp = -1;
+    
+    while (1) {
+      if (temp != value) {
+        temp = value;
+        ruleText = (value == 0) ? "Left" : "Right";
+        text(paramName + ": " + ruleText, 2, 3);
+      }
+
+      byte x = push(button3);
+      byte r = push(button1);
+
+      if (r && r == 1) {
+        value = (value == 0) ? 1 : 0;
+      }
+      
+      if (x && x == 1) {
+        value = (value == 0) ? 1 : 0;
+      }
+
+      if (x == 2 || r == 2) {
+        EEPROM.write(eeprom_addr, value);
+        delay(10);
+        oled.set1X();
+        return;
+      }
+    }
+  } else {
+    value = EEPROM.read(eeprom_addr);
+    int temp = -1;
+    int limit = 255;
+    int step_size = (indicator == 4 || indicator == 5) ? 1 : 5;
+
+    while (1) {
+      if (temp != value) {
+        temp = value;
+        text("SET: " + String(value) + "    ", 5, 3);
+      }
+
+      byte x = push(button3);
+      byte r = push(button1);
+
+      if (r && r == 1) {
+        value += step_size;
+        if (value > limit) value = 0;
+      }
+
+      if (x && x == 1) {
+        value -= step_size;
+        if (value < 0) value = limit;
+      }
+
+      if (x == 2 || r == 2) {
+        EEPROM.write(eeprom_addr, value);
+        delay(10);
+        oled.set1X();
+        return;
+      }
+    }
+  }
+}
